c150181c259f8f79364596bab5d58805abc1fd99
Maria Gomez <maria_gg86@yahoo.es>
2021-08-28 15:45:11 +0200

    Limitada ruta /lucky/number para que solo se pueda consumir con el método GET

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class LuckyController 
 {
     /**
-     * @Route("/lucky/number")
+     * @Route("/lucky/number", methods = {"GET"})
      */
     public function number(): Response
 
